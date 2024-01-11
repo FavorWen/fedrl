9fa7952099c71af7569d628a91ac25c1468d0806
FavorWen <ls_box@163.com>
2024-01-11 09:06:45 +0800

    analysis

--- rl/wait.sh
+++ rl/wait.sh
@@ -4,9 +4,9 @@
 partition=(iid)
 datasets=(CIFAR10 CIFAR100)
 archs=(CNN RESNET18)
-hdims=(4 4)
-cn=(25 5)
-pn=(5 3)
+hdims=(8)
+cn=(100)
+pn=(10)
 seeds=(5 4 3 2)
 # partition=(iid label-skew)
 # datasets=(FMNIST MNIST CIFAR10 CIFAR100)
@@ -17,25 +17,24 @@ seeds=(5 4 3 2)
 # seeds=(5 4 3 2)
 
 for part in "${partition[@]}"; do
-    for((i=0;i<4;i++))
+    for((i=0;i<2;i++))
     do
         dataset=${datasets[$i]}
         arch=${archs[$i]}
-        for((j=0;j<3;j++))
+        for((j=0;j<2;j++))
         do
             hdim=${hdims[$j]}
             c=${cn[$j]}
             p=${pn[$j]}
             for seed in "${seeds[@]}"; do
-                nohup python main_rpm.py --history_dim $hdim --client_nums $c --participant_nums $p --seed $seed --dataset $dataset --arch $arch --partition $part --optimizer SGD --lr 0.01 --epoch 1 --rl_ddl 200 --batch_size 32 > res-${arch}-${c}-seed${seed}.log &
+                python main_rpm.py --history_dim $hdim --client_nums $c --participant_nums $p --seed $seed --dataset $dataset --arch $arch --partition $part --optimizer SGD --lr 0.01 --epoch 1 --rl_ddl 200 --batch_size 32 > res-${arch}-${c}-seed${seed}.log &
                 echo "nohup python main_rpm.py --history_dim $hdim --client_nums $c --participant_nums $p --seed $seed --dataset $dataset --arch $arch --partition $part --optimizer SGD --lr 0.01 --epoch 1 --rl_ddl 200 --batch_size 32 > res-${arch}-${c}-seed${seed}.log &"
             done
-            wait -n
+            wait
+            echo ""
         done
     done
 done
-
-
 
 
 # sleep 40m
